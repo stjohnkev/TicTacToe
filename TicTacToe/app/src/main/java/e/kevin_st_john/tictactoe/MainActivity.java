17a3f41d119e957407a92e8b123c833869e8bfa8
stjohnkev <43213852+stjohnkev@users.noreply.github.com>
2018-10-07 13:42:05 +0100

    Finished View

--- TicTacToe/app/src/main/java/e/kevin_st_john/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/e/kevin_st_john/tictactoe/MainActivity.java
@@ -2,6 +2,9 @@ package e.kevin_st_john.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mGame = new TicTacToeGame(this);
+    }
+
+    public void pressedSquare(View view){
+        String tagAsStr = view.getTag().toString();
+        int tagAsInt = Integer.parseInt(tagAsStr);
+       // Log.d("TTT", "You have pressed index " + tagAsInt);
+
+        //Toast.makeText(this, "You pressed index " + tagAsInt, Toast.LENGTH_SHORT).show();
+    }
+
+    public void pressedNewGame(View view){
+        String tagAsStr = view.getTag().toString();
+        int tagAsInt = Integer.parseInt(tagAsStr);
+        // Log.d("TTT", "You have pressed index " + tagAsInt);
+
+        //Toast.makeText(this, "You pressed index " + tagAsInt, Toast.LENGTH_SHORT).show();
     }
 
 }
